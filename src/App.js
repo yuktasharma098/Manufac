6b0ce0967f6ea71291f7e8db2c27366e7b12a2ae
yuktasharma098 <sharmayukta098@gmail.com>
2023-04-30 20:25:07 +0530

    app.js changes

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import BarChart from "./components/BarChart";
 import LineChart from "./components/LineChart";
-//this functio render both the charts which are present in component named LineChart and BarChart
+//this functional component render both the charts which are present in component named LineChart and BarChart
 function App() {
 
   return (
