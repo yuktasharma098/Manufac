76bba5d5a036bc32dd3854b590005491b24fb339
yuktasharma098 <sharmayukta098@gmail.com>
2023-04-30 20:19:47 +0530

    changes related to comment

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -3,7 +3,9 @@ import data from '../Wine-Data.json'
 import * as echarts from 'echarts';
 
 const BarChart = () => {
+  //useEffect is used so as to add the data to the chart directly as the component render
   useEffect(() => {
+    //id is given so as to render the chart using div tag
     const barChart = echarts.init(document.getElementById('barChart'));
     barChart.setOption({
       xAxis: {
@@ -15,6 +17,7 @@ const BarChart = () => {
         type: 'value',
         name: 'Magnesium'
       },
+      // type of the series is bar as bar chart is to be made and map render the data of magnesium present is array of object
       series: [{
         data: data.map((item) => item.Magnesium),
         type: 'bar'
@@ -34,7 +37,7 @@ const BarChart = () => {
   }, []);
 
   return (
-    <div id="barChart" style={{display:'flex', justifyContent:'center', width: '100%', height: '500px' }}></div>
+    <div id="barChart" style={{ display: 'flex', justifyContent: 'center', width: '100%', height: '500px' }}></div>
   );
 }
 
--- src/components/LineChart.js
+++ src/components/LineChart.js
@@ -4,7 +4,9 @@ import { useEffect } from 'react';
 import * as echarts from 'echarts';
 
 const LineChart = () => {
+  //useEffect is used so as to add the data to the chart directly as the component render
   useEffect(() => {
+    //id is given so as to render the chart using div tag
     const lineChart = echarts.init(document.getElementById('lineChart'));
     lineChart.setOption({
       xAxis: {
@@ -16,6 +18,7 @@ const LineChart = () => {
         type: 'value',
         name: 'Ash'
       },
+      // type of the series is line as line chart is to be made and map render the data of magnesium present is array of object
       series: [{
         data: data.map((item) => item.Ash),
         type: 'line'
@@ -35,7 +38,7 @@ const LineChart = () => {
   }, []);
 
   return (
-    <div id="lineChart" style={{display:'flex', justifyContent:'center', width: '100%', height: '500px' }}></div>
+    <div id="lineChart" style={{ display: 'flex', justifyContent: 'center', width: '100%', height: '500px' }}></div>
   );
 }
 
